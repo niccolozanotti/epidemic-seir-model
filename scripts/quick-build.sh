315758d8bcbfe4ebcbf84d5bb06c79f4cc5ca040
Niccolo Zanotti <niccolo.zanotti@icloud.com>
2021-07-09 23:01:50 +0200

    Sripts updated with user friendly message

--- scripts/quick-build.sh
+++ scripts/quick-build.sh
@@ -5,3 +5,5 @@ fi
 cd ../sim_build || exit
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make
+echo "All built targets are in sim_build directory"
+echo "(use cd ../sim_build to checkout)"
--- scripts/testing.sh
+++ scripts/testing.sh
@@ -5,3 +5,5 @@ fi
 cd ../test_build || exit
 cmake  ..  -DCMAKE_BUILD_TYPE=Debug
 make test1 test2 test3
+echo "All built test targets are in test_build directory"
+echo "(use cd ../test_build to checkout)"
